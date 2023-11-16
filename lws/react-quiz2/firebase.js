80829e53d03e5af452857f36573545d03502c210
abu-sani <abu.sani@byslglobal.com>
2023-11-16 19:00:48 +0600

    add fire base

---
+++ lws/react-quiz2/firebase.js
@@ -0,0 +1,36 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCJea2KsPWJqX1q-wgwf4Dky6MeoOL-CHg",
+//   authDomain: "react-quiz-dev-e5857.firebaseapp.com",
+//   projectId: "react-quiz-dev-e5857",
+//   storageBucket: "react-quiz-dev-e5857.appspot.com",
+//   messagingSenderId: "935613585184",
+//   appId: "1:935613585184:web:22bab245d8037d0d306945",
+//   measurementId: "G-P4X47334ZN"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+import { initializeApp } from 'firebase/app';
+
+
+const app = initializeApp({
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+});
+
+export default app;
