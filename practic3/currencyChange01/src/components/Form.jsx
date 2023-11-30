dc6f7144485edd84f51d280c558bbfac60d2217b
abu-sani <abu.sani@byslglobal.com>
2023-11-30 18:59:08 +0600

    from making

--- practic3/currencyChange01/src/components/Form.jsx
+++ practic3/currencyChange01/src/components/Form.jsx
@@ -3,13 +3,30 @@ import React from 'react';
 function Form(props) {
     return (
         <div>
-            <div>
-                <form>
-                    <div>
-                        <label>First name:</label>
-                    </div>
-                </form>
-            </div>
+            <form>
+                <div>
+                    <label>Currency: </label>
+                    <select name="cars" id="cars">
+                        <option value="volvo">Volvo</option>
+                        <option value="saab">Saab</option>
+                        <option value="mercedes">Mercedes</option>
+                        <option value="audi">Audi</option>
+                    </select>
+                    <input type="number" />
+                </div>
+
+                <div>
+                    <label>Currency: </label>
+                    <select name="cars" id="cars">
+                        <option value="volvo">Volvo</option>
+                        <option value="saab">Saab</option>
+                        <option value="mercedes">Mercedes</option>
+                        <option value="audi">Audi</option>
+                    </select>
+                    <input type="number" />
+                </div>
+                <button type="button">Convert</button>
+            </form>
         </div>
     );
 }
