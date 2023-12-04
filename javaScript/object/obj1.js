6ea6cb7c4a2833363fa830eab466e43d4f25938b
abu-sani <abu.sani@byslglobal.com>
2023-12-04 19:08:52 +0600

    currency conveter 2

--- javaScript/object/obj1.js
+++ javaScript/object/obj1.js
@@ -87,3 +87,95 @@
 
 
 // ---------------------
+// const products = document.getElementById("demo");
+//
+// // products.innerText = 'test'
+// async function getText() {
+//     const res = await fetch('https://dummyjson.com/products/1');
+//     const data = await res.text();
+//     products.innerText = data;
+// }
+
+// let dataP = getText()
+// console.log(dataPro)
+
+// -----------------------------
+
+// let data = [];
+// const x = async function () {
+//     let obj;
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+//     obj = await res.json();
+//     // data = obj
+//     // return obj;
+//     // console.log(obj)
+// }
+//
+// // const products = x();
+// console.log(x())
+
+// ---------------------------------------
+
+// let data = [];
+//
+// async function getRandomUser(){
+//     // gets the response from the api and put it inside a constant
+//     const response = await fetch('https://randomuser.me/api');
+//     //the response have to be converted to json type file, so it can be used
+//     const data = await response.json();
+//     //the addData adds the object "data" to an array
+//     addData(data)
+// }
+//
+// function addData(object) {
+//     // the push method add a new item to an array
+//     // here it will be adding the object from the function getRandomUser each time it is called
+//     data.push(object);
+//     //the fetched data is available only on this scope
+//     // console.log("This is the value of date inside the function addData:")
+//     // console.log(data)
+// }
+//
+// //Calls the function that fetches the data
+// getRandomUser()
+//
+// // console.log("This is the value of data outside the scope")
+// console.log(data)
+
+// ------------------------------------------
+
+// let data = []
+// const dataF = function (success) {
+//     //do what you want HERE.
+//     console.log(success)
+//     data.push(success)
+//     return data;
+// }
+//
+// fetch('https://reqres.in/api/users?page=2')
+//     .then(data => data.json())
+//     .then(success => dataF(success));
+//
+// console.log(dataF())
+
+// -------------------------------------------
+
+// Store database to array
+// let database = []
+//
+//
+// async  function my() {
+//     const response =await fetch('https://reqres.in/api/users?page=2')
+//     var data = await response.json();
+//     database.push(data)
+//     print(database)
+// }
+// console.log(my())
+// function print(data){
+//
+//     console.log(data)
+// }
+//
+// console.log("aadasd",database)
+
+// ----------------------
--- practic3/currencyChange01/src/components/TestComponents.jsx
+++ practic3/currencyChange01/src/components/TestComponents.jsx
@@ -1,12 +1,40 @@
+import React, {useState} from 'react';
 import CurrencyCall from "./CurrencyCall.js";
 
-import React from 'react';
+function selectHandler(currency1,currency2,currencyFirstValue){
+    console.log(currency1,currency2,currencyFirstValue);
+}
+function TestComponents() {
+    let [currency1, setCurrency1] = useState("usd")
+    let [currency2, setCurrency2] = useState("bdt")
 
-function TestComponents(props) {
-    let currencyVal = CurrencyCall('usd')
-    console.log(typeof currencyVal)
+    let currencyValue1 = CurrencyCall(currency1);
+    let currencyKey1 = Object.keys(currencyValue1);
+
+    let currencyValue2 = CurrencyCall(currency2);
+    let currencyKey2 = Object.keys(currencyValue2);
+
+    let [currencyFirstValue, setCurrencyFirstValue] = useState(0)
+
     return (
         <div>
+            <select value={currency1} onChange={(e)=>setCurrency1(e.target.value)}>
+                {currencyKey1.map((option) => (
+                    <option key={option} value={option}>{option}</option>
+                ))}
+            </select>
+            <input type="number" value={currencyFirstValue} onChange={(e)=>setCurrencyFirstValue(e.target.value)} />
+            <br />
+            <select value={currency2} onChange={(e)=>setCurrency2(e.target.value)}>
+                {currencyKey2.map((option) => (
+                    <option key={option}>{option}</option>
+                ))}
+            </select>
+            <input type="number" disabled/>
+            <br />
+            <button onClick={()=>selectHandler(currency1,currency2,currencyFirstValue)}>
+                convert
+            </button>
 
         </div>
     );
