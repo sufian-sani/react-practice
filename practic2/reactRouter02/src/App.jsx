6933144ea533218183d63dfe0040fa48cfaf2778
abu-sani <abu.sani@byslglobal.com>
2023-12-06 18:25:35 +0600

    route 3

---
+++ practic2/reactRouter02/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import {Outlet} from 'react-router-dom'
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <Header />
+        <Outlet />
+        <Footer />
+    </>
+  )
+}
+
+export default App
