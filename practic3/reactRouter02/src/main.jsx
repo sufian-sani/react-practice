820d48d015b507c0d1bfe4aab94e1bdb114941a1
abu-sani <abu.sani@byslglobal.com>
2023-12-20 20:17:51 +0600

    react contextapi 11

---
+++ practic3/reactRouter02/src/main.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import Home from "./components/Pages/Home.jsx";
+import About from "./components/Pages/About.jsx";
+import Contact from "./components/Pages/Contact.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/about",
+                element: <About />
+            },
+            {
+                path: "/contact",
+                element: <Contact />
+            }
+        ]
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <RouterProvider router={router} />
+  </React.StrictMode>,
+)
