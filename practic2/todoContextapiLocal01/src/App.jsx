a8df3d7ca540ee9a7a05a6de1b698c4b4784d408
abu-sani <abu.sani@byslglobal.com>
2023-12-26 19:12:28 +0600

    react contexapi todo 2

--- practic2/todoContextapiLocal01/src/App.jsx
+++ practic2/todoContextapiLocal01/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -22,6 +22,18 @@ function App() {
     const toggleComplete = (id) => {
         setTodos((prev)=>prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo ))
     }
+
+    useEffect(() => {
+        const todos = JSON.parse(localStorage.getItem("todos"))
+
+        if (todos && todos.length > 0) {
+            setTodos(todos)
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos))
+    }, [todos])
 
   return (
       <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
