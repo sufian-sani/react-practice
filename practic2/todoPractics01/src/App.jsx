06e88247d52823313bdefb768364a3f652c3d5ad
abu-sani <abu.sani@byslglobal.com>
2023-12-27 19:01:15 +0600

    react contexapi todo 3

---
+++ practic2/todoPractics01/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Forms from "./components/Forms.jsx";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Todo List:</h1>
+        <Forms />
+    </>
+  )
+}
+
+export default App
