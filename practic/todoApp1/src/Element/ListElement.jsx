f1a7f15bb7e9b50893e23db6e051de76ae2fd7f9
abu-sani <abu.sani@byslglobal.com>
2024-02-08 19:01:03 +0600

    new react todo 1

--- practic/todoApp1/src/Element/ListElement.jsx
+++ practic/todoApp1/src/Element/ListElement.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ButtonElement from "./ButtonElement.jsx";
 const ListElement = (props) => {
-    const todoIs = (id) => {
-        console.log(id)
-    }
     return (
         <ul>
             {props.listItem.map((todo,index) => (
-                <li key={index} onClick={todoIs(todo.id)}>{todo.text} {props.removeButtonComponents}</li>
+                <li key={index}>{todo.text} {props.removeButtonComponents}</li>
             ))}
         </ul>
     );
