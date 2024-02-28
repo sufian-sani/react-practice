15b338410b0e8696152721352a5a6284a5746187
abu-sani <abu.sani@byslglobal.com>
2024-02-28 17:44:51 +0600

    accordind function 2 fixed

--- practic2/vitereact/src/components/accordian2/index.jsx
+++ practic2/vitereact/src/components/accordian2/index.jsx
@@ -3,25 +3,50 @@ import data from "../data.js";
 
 function According2(props) {
     const [selected, setSelected] = useState(null);
+    const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+    const [multiple, setMultiple] = useState([]);
 
     function handleSingleSelection(getId){
-        console.log(getId)
+        setSelected(getId===selected?null:getId)
+    }
+
+    function handleMultiSelection(getId){
+        let cpyMultiple = [...multiple]
+        const findIndexOfCurrentId = cpyMultiple.indexOf(getId)
+        if(findIndexOfCurrentId === -1) cpyMultiple.push(getId)
+        else cpyMultiple.splice(findIndexOfCurrentId, 1)
+        setMultiple(cpyMultiple)
     }
 
     return (
         <div>
+            <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>Enable Multi Selection</button>
             <div>
                 {data && data.length>0 ? (
                     data.map((dataItem)=>(
                         <div>
-                            <div onClick={() => handleSingleSelection(dataItem.id)}>
+                            <div onClick={
+                                enableMultiSelection
+                                ? () => handleMultiSelection(dataItem.id)
+                                : () => handleSingleSelection(dataItem.id)
+                            }>
                                 <h3>{dataItem.question}</h3>
                                 <span>+</span>
                             </div>
                             {
-                                selected
+                                enableMultiSelection
+                                ? multiple.indexOf(dataItem.id) !== -1 && (
+                                    <div className="content">{dataItem.answer}</div>
+                                ) : selected === dataItem.id && (
+                                    <div className="content">{dataItem.answer}</div>
+                                )
                             }
-                            {/*<div className="content">{dataItem.answer}</div>*/}
+                            {/*{*/}
+                            {/*    selected === dataItem.id || multiple.indexOf(dataItem.id) !== -1 ? (*/}
+                            {/*            <div className="content">{dataItem.answer}</div>*/}
+                            {/*        )*/}
+                            {/*        :null*/}
+                            {/*}*/}
                         </div>
                     ))
                 ) : (
