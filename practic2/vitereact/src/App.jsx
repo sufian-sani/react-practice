91802fdef56e19eef50ee63463a7b6722f06c3e2
abu-sani <abu.sani@byslglobal.com>
2024-03-13 17:46:07 +0600

    menu tree

--- practic2/vitereact/src/App.jsx
+++ practic2/vitereact/src/App.jsx
@@ -1,21 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 import According from "./components/accordian/index.jsx";
 import According2 from "./components/accordian2/index.jsx";
 import RandomColor from "./components/random-color/index.jsx";
 import RandomColor2 from "./components/random-color2/RandomColor2.jsx";
 // import StarRating from "./components/star-rating/index.jsx";
-import StarRating from "./components/star-rating2/StarRating.jsx";
-import Accordian from "./components2/accordian/Accordian.jsx";
-import ColorGenerate from "./components2/colorgenerator/ColorGenerate.jsx";
-import StarFeed from "./components2/starfeedback/index.jsx";
+// import StarRating from "./components/star-rating2/StarRating.jsx";
+// import Accordian from "./components2/accordian/Accordian.jsx";
+// import ColorGenerate from "./components2/colorgenerator/ColorGenerate.jsx";
+// import StarFeed from "./components2/starfeedback/index.jsx";
 // import ImageSlider from "./components/image-slider/index.jsx";
 // import ImageSlider from "./components/image-slider2/index.jsx";
-import ImageSlider from "./components/image-slider2/indexAi.jsx";
-import LoadMoreData from "./components/load-more-data/LoadMoreData.jsx";
+// import ImageSlider from "./components/image-slider2/indexAi.jsx";
+// import LoadMoreData from "./components/load-more-data/LoadMoreData.jsx";
+// import menus from "./components/tree-view/data";
+// import TreeView from "./components/tree-view/index.jsx";
 
+import menus from "./components/tree-view2/data.js";
+import TreeView2 from "./components/tree-view2/index.jsx";
 function App() {
 
   return (
@@ -31,7 +35,9 @@ function App() {
       {/*  <StarFeed />*/}
       {/*  <ImageSlider url={"https://picsum.photos/v2/list"} page={'1'} limit={"10"} />*/}
       {/*  <ImageSlider />*/}
-        <LoadMoreData />
+      {/*  <LoadMoreData />*/}
+      {/*  <TreeView  menus={menus} />*/}
+        <TreeView2 menus={menus} />
     </div>
   );
 }
