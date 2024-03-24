e0720ecd923fa63d0e125ddc246902a553bb5ff5
abu-sani <abu.sani@byslglobal.com>
2024-03-24 16:34:16 +0600

    24-03

---
+++ lws/react-quiz-3/components/Brand.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Brand(props) {
+    return (
+        <ul>
+            <li>
+                <a href="index.html" className="brand">
+                    <img src="./images/logo-bg.png" alt="Learn with Sumit Logo"/>
+                    <h3>Learn with Sumit</h3>
+                </a>
+            </li>
+        </ul>
+    );
+}
+
+export default Brand;
