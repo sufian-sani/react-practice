2d1fa5de3d3e38dc7934e6555571ae73a79f0bdf
abu-sani <abu.sani@byslglobal.com>
2024-03-24 19:05:00 +0600

    24-03 7:04

---
+++ practic/blog-full/models/Post.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../sequelize');
+
+// Define Post model
+const Post = sequelize.define('Post', {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    author: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    }
+});
+
+module.exports = Post;
---
+++ practic/blog-full/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const sequelize = require('./sequelize');
+const postsRouter = require('./routes/posts');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.use('/posts', postsRouter);
+
+// Sync Sequelize models with the database
+sequelize.sync()
+    .then(() => {
+        console.log('Database synchronized');
+        // Start server
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch(error => {
+        console.error('Error synchronizing database:', error.message);
+    });
