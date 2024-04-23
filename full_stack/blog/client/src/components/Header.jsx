13beca35abac94a179b8aec13f9887fecd1eff15
abu-sani <abu.sani@byslglobal.com>
2024-04-23 19:05:48 +0600

    21-04 6:13

--- full_stack/blog/client/src/components/Header.jsx
+++ full_stack/blog/client/src/components/Header.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import {useSelector} from 'react-redux'
 
 const Header = () => {
+    const isLogin = useSelector(state => state.isLogin)
+    console.log(isLogin)
     return (
         <div>
             <h4>My Blog App</h4>
-            <button LinkComponent={Link} to="/blogs" >Home</button>
-            <button LinkComponent={Link} to="/login" >Login</button>
-            <button LinkComponent={Link} to="/register" >Register</button>
-            <button>Logout</button>
+
+            {isLogin && (
+                <button LinkComponent={Link} to="/blogs">Home</button>
+            )}
+
+            {!isLogin && (
+                <>
+                    <button LinkComponent={Link} to="/login">Login</button>
+                    <button LinkComponent={Link} to="/register">Register</button>
+                </>
+            )}
+
+            {isLogin && (
+                <button>Logout</button>
+            )}
         </div>
     );
 };
