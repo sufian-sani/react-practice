659e6e1f535773df53ebf3e6d68927fed1890874
abu-sani <abu.sani@byslglobal.com>
2024-04-28 19:13:17 +0600

    04-28 7:13

---
+++ full_stack/blog_1/controllers/blogControlller.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+const blogModel = require("../models/blogModel");
+const userModel = require("../models/userModel");
+
+//GET ALL BLOGS
+exports.getAllBlogsController = async (req, res) => {
+    try {
+        const blogs = await blogModel.find({}).populate("user");
+        if (!blogs) {
+            return res.status(200).send({
+                success: false,
+                message: "No Blogs Found",
+            });
+        }
+        return res.status(200).send({
+            success: true,
+            BlogCount: blogs.length,
+            message: "All Blogs lists",
+            blogs,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            success: false,
+            message: "Error WHile Getting Blogs",
+            error,
+        });
+    }
+};
+
+//Create Blog
+
+exports.createBlogController = async (req, res) => {
+    try {
+        const { title, description, image, user } = req.body;
+        //validation
+        if (!title || !description || !image || !user) {
+            return res.status(400).send({
+                success: false,
+                message: "Please Provide ALl Fields",
+            });
+        }
+        const exisitingUser = await userModel.findById(user);
+        //validaton
+        if (!exisitingUser) {
+            return res.status(404).send({
+                success: false,
+                message: "unable to find user",
+            });
+        }
+
+        const newBlog = new blogModel({ title, description, image, user });
+        const session = await mongoose.startSession();
+        session.startTransaction();
+        await newBlog.save({ session });
+        exisitingUser.blogs.push(newBlog);
+        await exisitingUser.save({ session });
+        await session.commitTransaction();
+        await newBlog.save();
+        return res.status(201).send({
+            success: true,
+            message: "Blog Created!",
+            newBlog,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send({
+            success: false,
+            message: "Error WHile Creting blog",
+            error,
+        });
+    }
+};
--- full_stack/blog_1/server.js
+++ full_stack/blog_1/server.js
@@ -3,14 +3,27 @@ const cors = require('cors');
 const morgan = require('morgan');
 const colors = require('colors');
 const dotenv = require('dotenv');
+const connectDB = require("./config/db");
 
 dotenv.config();
+
+
+//router import
+const userRoutes = require("./routes/userRoutes");
+const blogRoutes = require("./routes/blogRoutes");
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+
+
+//routes
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/blog", blogRoutes);
 
 app.get('/', (req, res) => {
     res.status(200).send({
